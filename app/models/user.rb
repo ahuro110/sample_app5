109561a27d8cf28f93638535418f6db01479113f
伊藤脩一郎 <ahuro110.play@gmail.com>
2020-05-28 01:52:28 +0900

    finish_favorite_comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  has_many :post_images, dependent: :destroy
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
+          :recoverable, :rememberable, :validatable
+  has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  end
