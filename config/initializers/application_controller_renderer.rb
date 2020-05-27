42d6cb835db9359360a6a1d22924934ca3bde323
伊藤脩一郎 <ahuro110.play@gmail.com>
2020-05-27 22:31:13 +0900

    finish_post_image

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'dfd8c7c3ad877f7b82e6e6ff3077dc8d7c4a178318d8f29845eeff48ab74053fc0788bb7fb192889113e41c18283ebec84155fd5fc2f144e5579ecab3c89bef4'
